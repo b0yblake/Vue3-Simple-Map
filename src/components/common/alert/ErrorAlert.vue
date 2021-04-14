54a3b78fcc60e6d3672f154b05e84fd3ee67bbde
b0yblake <lichnh@hivelab.com>
2021-04-14 23:36:38 +0700

    update components

---
+++ src/components/common/alert/ErrorAlert.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-show="isError" class="error-alert border border-red-700 bg-red-200 p-2 my-2">
+    {{ content }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ErrorAlert',
+  props: {
+    isError: {
+      type: Boolean,
+    },
+    content: {
+      type: String,
+      default: "Oops!! Some thing went wrong!"
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/UserLocation.vue
+++ src/views/UserLocation.vue
@@ -1,20 +1,32 @@
 <template>
   <section class="user-location border border-gray-400 rounded p-4">
-    <div class="form-wrap flex flex-col">
-      <h2 class="title text-black-500 text-lg uppercase ">Google map direction 🌍</h2>
-      
-      <TextForm :id="'form-address'" :isBlind="true" :label="'Enter Your Address'" />
-      <TextButton :title="'Go'" />
-    </div>
+    <form @click.prevent="submitForm()">
+      <div class="form-wrap flex flex-col">
+        <h2 class="title text-black-500 text-lg uppercase ">Google map direction 🌍</h2>
+        <ErrorAlert :isError="error" :content="'Oops!! Some thing went wrong!'" />
+        <TextForm :id="'form-address'" :isBlind="true" :label="'Enter Your Address'" />
+        <TextButton :title="'Go'" />
+      </div>
+    </form>
     <div class="map">
-      
+
     </div>
   </section>
 </template>
 
 <script>
 export default {
-
+  name: "",
+  data() {
+    return {
+      error: false,
+    }
+  },
+  methods: {
+    submitForm() {
+      console.log('submit form - not reload form');
+    }
+  }
 }
 </script>
 
