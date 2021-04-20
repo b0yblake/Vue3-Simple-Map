1636ba348e2442382ce014070352b01732411664
b0yblake <lichnh@hivelab.com>
2021-04-20 11:01:52 +0700

    up

--- src/services/googleNearBySearch.js
+++ src/services/googleNearBySearch.js
@@ -7,7 +7,20 @@ export const nearBySearch = async (optionSearchBy = {
   typeRange: 1,
 }) =>
   await fetch(
-    `${API_URL}/place/nearbysearch/json?location=${optionSearchBy.latitude},${optionSearchBy.longitude}&type=${optionSearchBy.typeSearch}&radius=${optionSearchBy.typeRange * 1000}&key=${API_KEY}`
+    `${API_URL}/place/nearbysearch/json?location=${optionSearchBy.latitude},${optionSearchBy.longitude}&type=${optionSearchBy.typeSearch}&radius=${optionSearchBy.typeRange * 1000}&key=${API_KEY}`,
+    {
+      method: 'GET', // *GET, POST, PUT, DELETE, ..
+      mode: 'cors', // *cors, no-cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // *same-origin, include, omit
+      headers: {
+        //'Content-Type': 'application/json' // application/json, application/x-www-form-urlencoded
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // *follow, manual, error
+      referrerPolicy: 'no-referrer', // *no-referrer-when-downgrade, no-referrer, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      //body: JSON.stringify(data) // body data type must match "Content-Type" header
+    }
   )
   .then(
     function(response) {
